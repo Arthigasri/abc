2d1a687c8b7352afdec9b775a0c5aaeb85775acd
Arthigasri <arthi1461997@gmail.com>
2017-07-07 09:43:26 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,22 @@
+# abc
+
+import java.util.*;
+import java.io.*; 
+public class Fact
+{
+  
+  public static void main(String args[])
+  {
+    
+    Scanner sc=new Scanner(System.in);
+    int i,fact=1;
+    int num=5;
+    for(i=1;i<=num;i++)
+    {
+      
+      fact=fact*i;
+                  }
+    System.out.println(fact);
+    }
+    }
+
