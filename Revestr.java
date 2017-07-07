0a36f0acd7660ebb3d4ac175baf3a474b021ccb4
Arthigasri <arthi1461997@gmail.com>
2017-07-07 09:36:59 +0530

    Create Revestr.java

---
+++ Revestr.java
@@ -0,0 +1,18 @@
+# abc
+import java.util.*;
+import java.io.*; 
+public class Revestr
+{
+  
+  public static void main(String args[])
+  {
+    
+    Scanner sc=new Scanner(System.in);
+    int i;
+    String input="arthi";
+    char[] a=input.toCharArray();
+    for(i=a.length-1;i>=0;i--)
+    System.out.println(a[i]);
+    }
+    }
+
