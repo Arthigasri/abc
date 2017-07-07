afd9e9c21b3f1eb2f15e955c1530e8919a42c8d0
Arthigasri <arthi1461997@gmail.com>
2017-07-07 11:36:42 +0530

    Update and rename Reversenumber.java to Strtoint.java

---
+++ Strtoint.java
@@ -0,0 +1,17 @@
+# abc
+
+import java.util.*;
+import java.io.*; 
+public class Strtoint
+{   
+  public static void main(String args[])  
+  {
+  
+  Scanner s = new Scanner(System.in);
+    String a=s.nextLine();
+    int b=Integer.parseInt(a);
+    System.out.println(b);
+    }
+    }  
+ 
+  
